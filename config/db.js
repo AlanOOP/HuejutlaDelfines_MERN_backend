9d1bc03f5f772952a70384656b26d3671aeb2a45
AlanAlexis <delealli7js@gmail.com>
2024-10-23 14:46:57 -0600

    Refactorizar conexión a la base de datos

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 
 const conectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        // const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect('mongodb+srv://root:6Tq8lcc6R3gHADll@cluster0.xb2m3w4.mongodb.net/HuejutlaDB?retryWrites=true&w=majority')
         const url = ` ${conn.connection.host} : ${conn.connection.port} `;
         // console.log(`MongoDB connected: ${url} ✅`);
     } catch (error) {
